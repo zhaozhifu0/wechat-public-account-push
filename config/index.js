60498bc16a184e0a9288ecdbb1749866cc608ec5
无聊才读书 <576446831@qq.com>
2022-09-23 16:43:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,21 +60,21 @@ export const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "onPME54pkPJW6fEg88pX9XDlbkvw,onPME55QDi1btWM34hSKiIRgPwy8",
+      name: "努力的人",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "qcby7ZqhiBVfa1aW3Ly8X9iDsINZWfXML3rhpiLvGx8",
+      id: "onPME54pkPJW6fEg88pX9XDlbkvw",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "qcby7ZqhiBVfa1aW3Ly8X9iDsINZWfXML3rhpiLvGx8",
       // 所在省份
-      province: "广东",
+      province: "黑龙江",
       // 所在城市
-      city: "肇庆",
+      city: "哈尔滨",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '11-19',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "http://www.zhaozf.site",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -96,40 +96,17 @@ export const USER_CONFIG = {
     },
     {
       name: "老婆1",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: "https://wangxinleo.cn",
+      id: "onPME55QDi1btWM34hSKiIRgPwy8",
+      useTemplateId: "qcby7ZqhiBVfa1aW3Ly8X9iDsINZWfXML3rhpiLvGx8",
+      province: "吉林",
+      city: "长春",
+      horoscopeDate: '12-21',
+      horoscopeDateType: '今日',
+      openUrl: "http://www.zhaozf.site",
       festivals: [],
       customizedDateList: []
     },
-    {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
-    },
-    {
-      name: "老婆3",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      horoscopeDate: '',
-      horoscopeDateType: '',
-      openUrl: "https://wangxinleo.cn",
-      festivals: [],
-      customizedDateList: []
-    },
+   
     // 你可以不断按格式往下增加
     // ...
   ],
@@ -160,9 +137,9 @@ export const USER_CONFIG = {
   /** 天气相关 */
   
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "黑龙江",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "哈尔滨",
   
   /** 重要节日相关 */
   
